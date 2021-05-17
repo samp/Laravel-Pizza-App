18c818ad3f5722f58fb6096da41fcf6f30245f77
samp <isambarding@gmail.com>
2021-05-17 16:01:08 +0100

    rate limiting

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/order', [App\Http\Controllers\OrderController::class, 'index'])->name('order');
+Route::get('/order', [App\Http\Controllers\OrderController::class, 'index'])->middleware(['throttle:limitpizza'])->name('order');
 Route::get('/cart', [App\Http\Controllers\CartController::class, 'index'])->name('cart');
 Route::get('/deals', [App\Http\Controllers\DealController::class, 'index'])->name('deals');
 Route::get('/success', [App\Http\Controllers\CartController::class, 'success'])->name('success')->middleware('auth');;
 
-Route::post('order', [App\Http\Controllers\OrderController::class, 'AddToCart']);
+Route::post('order', [App\Http\Controllers\OrderController::class, 'AddToCart'])->middleware(['throttle:limitpizza']);
 Route::post('deals', [App\Http\Controllers\DealController::class, 'AddRemoveDeal']);
 Route::post('cart', [App\Http\Controllers\CartController::class, 'SubmitOrder'])->middleware('auth');
 
